4727df19908a961576bc5178f843bbd5b25c64b8
Rosiel Victor <rosielvictor.dev@gmail.com>
2025-08-21 13:35:20 -0400

    feat: update sitemap URLs to reflect new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://sendany.app',
+      url: 'https://sendany.all.dev.br',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://sendany.app/about',
+      url: 'https://sendany.all.dev.br/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://sendany.app/privacy',
+      url: 'https://sendany.all.dev.br/privacy',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
